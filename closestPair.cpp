37d9c989a7494a0e862d0f7759111aec38e662e2
jjkaddoura <jjkaddoura@umail.ucsb.edu>
2017-01-21 17:59:51 -0800

    Command line interface basically done...atof() function is truncating the double value though. Need to think of a different way to convert

--- closestPair.cpp
+++ closestPair.cpp
@@ -2,26 +2,77 @@
 #include <fstream>
 using namespace std;
 
+struct Point {
+  double x;
+  double y;
+};
+
+bool isValidInput(char c);
+void printAllPoints();
+static const int MAX_ARRAY_SIZE = 100;
+static int numPoints = 0;
+Point* pointsArr = new Point[MAX_ARRAY_SIZE];
 
 
 int main(int argc, char** argv){
 	
+	string algorithmChoice;
+	// int numPoints = 0;
+	bool firstCoordinateOfPoint = true; 
+	
+	
+
 
   // READ INPUT FROM FILE
   string filename = "input.txt";
   ifstream file;
   file.open(filename, ifstream::in);
   string line;
+  char inputChar;
   while(getline(file,line)){
-    cout << line << endl;
+  	string point = "";
     for(int i = 0; i < line.length(); i++){
-      
+    	inputChar = line[i];
+    	if(!isValidInput(inputChar)) 
+    		continue;
+    
+    	if(inputChar != ' ' && inputChar != '\n' && i != line.length()-1 ) {
+    		point += inputChar;
+    	}
+    	else if(point.length() > 0){
+    		if(i == line.length()-1){
+    				cout << "GOOOD" << endl;
+    				point += inputChar;
+    			}
+    		if(firstCoordinateOfPoint) {
+    			// to get last digit of the coordinate
+    			
+    			double coordinate = atof(point.c_str());
+    			pointsArr[numPoints].x = coordinate;
+    			
+    			firstCoordinateOfPoint = false;
+    			point = "";
+    		}
+    		else{
+    			 pointsArr[numPoints].y = atof(point.c_str());
+    			 numPoints++;
+    			 firstCoordinateOfPoint = true;
+    			 point = "";
+    			// break;
+    		}
+    		
+    	}
+
     }
+
   }
   
   // Once input is gathered
   // begin algorithm
-  string algorithmChoice = argv[1];
+  if(argc > 2)
+  	algorithmChoice = argv[1];
+
+
   if(argc < 2){
     cout << "Please include an argument: brute, basic,"
 	 << " or optimal to indicate which method to use." << endl;
@@ -41,16 +92,20 @@ int main(int argc, char** argv){
     cout << "END OF FILE" << endl;
   }
   file.close();
+
+
+  printAllPoints();
 }
 
-struct Point {
-  double x;
-  double y;
-};		   
 
-
-class ClosestPair {
-  
-	Point** pointsArr;
-
-};
+void printAllPoints(){
+	cout << numPoints << endl;
+	for(int i =0; i < numPoints; i++){
+		cout << i << ". x: " << pointsArr[i].x << " y: " << pointsArr[i].y << endl;
+	}
+}
+bool isValidInput(char c){
+	if((c >='0' && c <= '9' ) || 
+		c == ' ' || c== '\n' || c == '.') return true;
+	return false;
+}
